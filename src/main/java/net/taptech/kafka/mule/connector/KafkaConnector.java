81d5386f45e98ebfc2eceacbf8eb58d74984c370
Greg Lawson <Gregory.Lawson@altisource.com>
2018-01-28 19:12:47 -0500

    Upgraded to version 1.1.0

--- src/main/java/net/taptech/kafka/mule/connector/KafkaConnector.java
+++ src/main/java/net/taptech/kafka/mule/connector/KafkaConnector.java
@@ -8,16 +8,13 @@ import net.taptech.kafka.mule.connector.config.ConnectorConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.RecordMetadata;
-import org.mule.api.MuleException;
 import org.mule.api.annotations.*;
 import org.mule.api.annotations.lifecycle.Start;
 import org.mule.api.annotations.lifecycle.Stop;
 import org.mule.api.annotations.param.Optional;
 import org.mule.api.callback.SourceCallback;
 import org.mule.api.endpoint.EndpointException;
-import org.mule.api.transport.ConnectorException;
 import org.mule.config.i18n.MessageFactory;
-import org.mule.util.concurrent.NamedThreadFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.io.DefaultResourceLoader;
@@ -32,193 +29,206 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  */
-@Connector(name="kafka", friendlyName="Kafka")
+@Connector(name = "kafka", friendlyName = "Kafka")
 public class KafkaConnector {
-	private static Logger logger = LoggerFactory.getLogger(KafkaConnector.class);
-	
-	@Config
-	ConnectorConfig config;
+    private static Logger logger = LoggerFactory.getLogger(KafkaConnector.class);
 
-	private Properties consumerProperties = new Properties();
-	private Properties producerProperties = new Properties();
-	private ExecutorService producerPool;
-	private ExecutorService consumerPool;
-	private static int connectorCount = 0;
-	private ResourceLoader resourceLoader = new DefaultResourceLoader();
+    @Config
+    ConnectorConfig config;
 
-	private static final int DEFAULT_POOL_SIZE = 5;
-	private static final long DEFAULT_THREAD_KEEP_ALIVE_TIME = 60L;
+    private Properties consumerProperties = new Properties();
+    private Properties producerProperties = new Properties();
+    private ExecutorService producerPool;
+    private ExecutorService consumerPool;
+    private static int connectorCount = 0;
+    private ResourceLoader resourceLoader = new DefaultResourceLoader();
 
-	static class KafkaConnectorNamedThreadFactory implements ThreadFactory {
-		private static final AtomicInteger poolNumber = new AtomicInteger(1);
-		private final ThreadGroup group;
-		private final AtomicInteger threadNumber = new AtomicInteger(1);
-		private final String namePrefix;
+    private static final int DEFAULT_POOL_SIZE = 5;
+    private static final long DEFAULT_THREAD_KEEP_ALIVE_TIME = 60L;
 
-		KafkaConnectorNamedThreadFactory(String name) {
-			SecurityManager s = System.getSecurityManager();
-			group = (s != null) ? s.getThreadGroup() :
-					Thread.currentThread().getThreadGroup();
-			namePrefix = name;
-		}
+    static class KafkaConnectorNamedThreadFactory implements ThreadFactory {
+        private static final AtomicInteger poolNumber = new AtomicInteger(1);
+        private final ThreadGroup group;
+        private final AtomicInteger threadNumber = new AtomicInteger(1);
+        private final String namePrefix;
 
-		public Thread newThread(Runnable r) {
-			Thread t = new Thread(group, r,
-					namePrefix + "-"+threadNumber.getAndIncrement(),
-					0);
-			if (t.isDaemon())
-				t.setDaemon(false);
-			if (t.getPriority() != Thread.NORM_PRIORITY)
-				t.setPriority(Thread.NORM_PRIORITY);
-			return t;
-		}
-	}
-	
-	@Start
-	public void initialize() throws Exception{
-		logger.info("KafkaConnector initialize called with count "+ ++connectorCount);
-		consumerProperties.load(resourceLoader.getResource(config.getConsumerPropertiesFile()).getInputStream());
-		producerProperties.load(resourceLoader.getResource(config.getProducerPropertiesFile()).getInputStream());
-		int producerThreads = determineThreads(producerProperties.getProperty(KafkaConnectorConstants.PRODUCER_THREADS));
-		int consumerThreads = determineThreads(consumerProperties.getProperty(KafkaConnectorConstants.CONSUMER_THREADS));
+        KafkaConnectorNamedThreadFactory(String name) {
+            SecurityManager s = System.getSecurityManager();
+            group = (s != null) ? s.getThreadGroup() :
+                    Thread.currentThread().getThreadGroup();
+            namePrefix = name;
+        }
 
-		producerPool = MoreExecutors.getExitingExecutorService(new ThreadPoolExecutor(producerThreads, Integer.MAX_VALUE,
-						DEFAULT_THREAD_KEEP_ALIVE_TIME, TimeUnit.SECONDS,
-						new SynchronousQueue<Runnable>(), new KafkaConnectorNamedThreadFactory("ProducerPool")),
-				100, TimeUnit.MILLISECONDS);
+        public Thread newThread(Runnable r) {
+            Thread t = new Thread(group, r,
+                    namePrefix + "-" + threadNumber.getAndIncrement(),
+                    0);
+            if (t.isDaemon())
+                t.setDaemon(false);
+            if (t.getPriority() != Thread.NORM_PRIORITY)
+                t.setPriority(Thread.NORM_PRIORITY);
+            return t;
+        }
+    }
+
+    @Start
+    public void initialize() throws Exception {
+        logger.info("KafkaConnector initialize called with count " + ++connectorCount);
+        consumerProperties.load(resourceLoader.getResource(config.getConsumerPropertiesFile()).getInputStream());
+        producerProperties.load(resourceLoader.getResource(config.getProducerPropertiesFile()).getInputStream());
+        int producerThreads = determineThreads(producerProperties.getProperty(KafkaConnectorConstants.PRODUCER_THREADS));
+        int consumerThreads = determineThreads(consumerProperties.getProperty(KafkaConnectorConstants.CONSUMER_THREADS));
+
+        producerPool = MoreExecutors.getExitingExecutorService(new ThreadPoolExecutor(producerThreads, Integer.MAX_VALUE,
+                        DEFAULT_THREAD_KEEP_ALIVE_TIME, TimeUnit.SECONDS,
+                        new SynchronousQueue<Runnable>(), new KafkaConnectorNamedThreadFactory("ProducerPool")),
+                100, TimeUnit.MILLISECONDS);
 		/*
 		producerPool = new ThreadPoolExecutor(producerThreads, Integer.MAX_VALUE,
 				DEFAULT_THREAD_KEEP_ALIVE_TIME, TimeUnit.SECONDS,
 				new SynchronousQueue<Runnable>(), new KafkaConnectorNamedThreadFactory("ProducerPool"));
 				*/
-		consumerPool =  MoreExecutors.getExitingExecutorService(new ThreadPoolExecutor(consumerThreads, Integer.MAX_VALUE,
-						DEFAULT_THREAD_KEEP_ALIVE_TIME, TimeUnit.SECONDS,
-						new SynchronousQueue<Runnable>(), new KafkaConnectorNamedThreadFactory("ConsumerPool")),
-				100, TimeUnit.MILLISECONDS);
-	}
+        consumerPool = MoreExecutors.getExitingExecutorService(new ThreadPoolExecutor(consumerThreads, Integer.MAX_VALUE,
+                        DEFAULT_THREAD_KEEP_ALIVE_TIME, TimeUnit.SECONDS,
+                        new SynchronousQueue<Runnable>(), new KafkaConnectorNamedThreadFactory("ConsumerPool")),
+                100, TimeUnit.MILLISECONDS);
+    }
 
-	private int determineThreads(String property) {
+    private int determineThreads(String property) {
 
-		int threads = DEFAULT_POOL_SIZE;
-		if (null != property){
-			try{
-				 threads = Integer.valueOf(property);
-			} catch( NumberFormatException nfe){
-				StringBuilder sb = new StringBuilder("Error converting ");
-				sb.append(property);
-				sb.append("to int value: ");
-				sb.append(nfe.getMessage());
-				sb.append(" Using default value of ").append(DEFAULT_POOL_SIZE);
-				sb.append(" for pool size.");
-				logger.warn(sb.toString());
-			}
-		}
-		return threads;
-	}
+        int threads = DEFAULT_POOL_SIZE;
+        if (null != property) {
+            try {
+                threads = Integer.valueOf(property);
+            } catch (NumberFormatException nfe) {
+                StringBuilder sb = new StringBuilder("Error converting ");
+                sb.append(property);
+                sb.append("to int value: ");
+                sb.append(nfe.getMessage());
+                sb.append(" Using default value of ").append(DEFAULT_POOL_SIZE);
+                sb.append(" for pool size.");
+                logger.warn(sb.toString());
+            }
+        }
+        return threads;
+    }
 
-	public Properties getConsumerProperties() {
-		return consumerProperties;
-	}
+    public Properties getConsumerProperties() {
+        return consumerProperties;
+    }
 
-	public Properties getProducerProperties() {
-		return producerProperties;
-	}
+    public Properties getProducerProperties() {
+        return producerProperties;
+    }
 
-	public ExecutorService getProducerPool() {
-		return producerPool;
-	}
+    public ExecutorService getProducerPool() {
+        return producerPool;
+    }
 
-	public ExecutorService getConsumerPool() {
-		return consumerPool;
-	}
+    public ExecutorService getConsumerPool() {
+        return consumerPool;
+    }
 
-	@Stop
-	public void shutdownAndAwaitTermination() {
-		ExecutorService [] servicesArray = {getProducerPool(), getConsumerPool()};
-		for (ExecutorService pool: Arrays.asList(servicesArray)){
-			pool.shutdown(); // Disable new tasks from being submitted
-			try {
-				// Wait a while for existing tasks to terminate
-				if (!pool.awaitTermination(DEFAULT_THREAD_KEEP_ALIVE_TIME, TimeUnit.SECONDS)) {
-					pool.shutdownNow(); // Cancel currently executing tasks
-					// Wait a while for tasks to respond to being cancelled
-					if (!pool.awaitTermination(DEFAULT_THREAD_KEEP_ALIVE_TIME, TimeUnit.SECONDS))
-						System.err.println("Pool did not terminate");
-				}
-			} catch (InterruptedException ie) {
-				// (Re-)Cancel if current thread also interrupted
-				pool.shutdownNow();
-				// Preserve interrupt status
-				Thread.currentThread().interrupt();
-			}
-		}
-	}
+    @Stop
+    public void shutdownAndAwaitTermination() {
+        ExecutorService[] servicesArray = {getProducerPool(), getConsumerPool()};
+        for (ExecutorService pool : Arrays.asList(servicesArray)) {
+            pool.shutdown(); // Disable new tasks from being submitted
+            try {
+                // Wait a while for existing tasks to terminate
+                if (!pool.awaitTermination(DEFAULT_THREAD_KEEP_ALIVE_TIME, TimeUnit.SECONDS)) {
+                    pool.shutdownNow(); // Cancel currently executing tasks
+                    // Wait a while for tasks to respond to being cancelled
+                    if (!pool.awaitTermination(DEFAULT_THREAD_KEEP_ALIVE_TIME, TimeUnit.SECONDS))
+                        System.err.println("Pool did not terminate");
+                }
+            } catch (InterruptedException ie) {
+                // (Re-)Cancel if current thread also interrupted
+                pool.shutdownNow();
+                // Preserve interrupt status
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
 
-	public ConnectorConfig getConfig() {
-		return config;
-	}
-	public void setConfig(ConnectorConfig config) {
-		this.config = config;
-	}
+    public ConnectorConfig getConfig() {
+        return config;
+    }
 
-	public static final Integer DEFAULT_DELAY = 1000;
+    public void setConfig(ConnectorConfig config) {
+        this.config = config;
+    }
 
-	private Properties copyProperties(Properties properties){
-		Properties newProperties = new Properties();
-		newProperties = (Properties) properties.clone();
-		return newProperties;
-	}
+    public static final Integer DEFAULT_DELAY = 1000;
 
-	@Source(name = "Consumer", friendlyName = "Consumer")
-	public void consumer(SourceCallback callback, @Optional String topic, @Optional String propertyFileOverrides) {
-		logger.info("consumer Creating simpleConsumer with propertyFileOverrides {}",propertyFileOverrides);
-		Integer delay = DEFAULT_DELAY;
-		String [] topicsArray = consumerProperties.getProperty(KafkaConnectorConstants.CONSUMER_TOPICS).split(",");
-		Properties properties = copyProperties(getConsumerProperties());
-		if (null != topic){
-			properties.put(KafkaConnectorConstants.CONSUMER_TOPICS, topic);
-		}
-		KafkaConsumerRunner runner = new KafkaConsumerRunner(properties, callback);
-		logger.debug("consumer Subscribing to topics {} with properties",topicsArray,properties);
-		consumerPool.submit(runner);
-	}
+    private Properties determineProperties(Properties properties, String propertyFileOverrides) {
+        Properties newProperties = new Properties();
+        if (null != propertyFileOverrides) {
+            try {
+                newProperties.load(resourceLoader.getResource(propertyFileOverrides).getInputStream());
+            } catch (Exception e) {
+                StringBuilder sb = new StringBuilder("Unable to load ");
+                sb.append(propertyFileOverrides).append(" property file.");
+                logger.warn(sb.toString(), e);
+                newProperties = (Properties) properties.clone();
+            }
+        } else {
+            newProperties = (Properties) properties.clone();
+        }
+
+        return newProperties;
+    }
+
+    @Source(name = "Consumer", friendlyName = "Consumer")
+    public void consumer(SourceCallback callback, @Optional String topic, @Optional String propertyFileOverrides) {
+        logger.info("consumer Creating simpleConsumer with propertyFileOverrides {}", propertyFileOverrides);
+        Integer delay = DEFAULT_DELAY;
+        String[] topicsArray = consumerProperties.getProperty(KafkaConnectorConstants.CONSUMER_TOPICS).split(",");
+        Properties properties = determineProperties(getConsumerProperties(), propertyFileOverrides);
+        if (null != topic) {
+            properties.put(KafkaConnectorConstants.CONSUMER_TOPICS, topic);
+        }
+        KafkaConsumerRunner runner = new KafkaConsumerRunner(properties, callback);
+        logger.debug("consumer Subscribing to topics {} with properties", topicsArray, properties);
+        consumerPool.submit(runner);
+    }
 
 
-	@Processor(name = "Seek", friendlyName = "Seek")
-	public List<ConsumerRecord<?, ?>> seek(int partition, String topic, int offset, @Optional String propertyFileOverrides) throws ExecutionException, InterruptedException {
-		logger.info("seek Creating simpleConsumer with partition {}, topic {}, offset {},  propertyFileOverrides {}",new Object[]{partition, topic, offset, propertyFileOverrides});
-		Integer delay = DEFAULT_DELAY;
+    @Processor(name = "Seek", friendlyName = "Seek")
+    public List<ConsumerRecord<?, ?>> seek(int partition, String topic, int offset, @Optional String propertyFileOverrides) throws ExecutionException, InterruptedException {
+        logger.info("seek Creating simpleConsumer with partition {}, topic {}, offset {},  propertyFileOverrides {}", new Object[]{partition, topic, offset, propertyFileOverrides});
+        Integer delay = DEFAULT_DELAY;
 
-		Properties properties = copyProperties(getConsumerProperties());
-		properties.put(KafkaConnectorConstants.CONSUMER_PARTITION, new Integer(partition));
+        Properties properties = determineProperties(getConsumerProperties(), propertyFileOverrides);
+        properties.put(KafkaConnectorConstants.CONSUMER_PARTITION, new Integer(partition));
         properties.put(KafkaConnectorConstants.CONSUMER_OFFSET, new Integer(offset));
         properties.put(KafkaConnectorConstants.CONSUMER_TOPIC, topic);
-        String [] topicsArray = properties.getProperty(KafkaConnectorConstants.CONSUMER_TOPICS).split(",");
-		if (null != topic){
-			properties.put(KafkaConnectorConstants.CONSUMER_TOPICS, topic);
-		}
-		KafkaConsumerOffsetRunner runner = new KafkaConsumerOffsetRunner(properties);
-		logger.debug("seek Subscribing to topics {} with properties",topicsArray,properties);
-		Future<List<ConsumerRecord<?, ?>>> future = consumerPool.submit(runner);
-		return future.get();
-	}
+        String[] topicsArray = properties.getProperty(KafkaConnectorConstants.CONSUMER_TOPICS).split(",");
+        if (null != topic) {
+            properties.put(KafkaConnectorConstants.CONSUMER_TOPICS, topic);
+        }
+        KafkaConsumerOffsetRunner runner = new KafkaConsumerOffsetRunner(properties);
+        logger.debug("seek Subscribing to topics {} with properties", topicsArray, properties);
+        Future<List<ConsumerRecord<?, ?>>> future = consumerPool.submit(runner);
+        return future.get();
+    }
 
-	
-	@Processor(name = "Producer", friendlyName = "Producer")
-	public List<RecordMetadata> producer(@Optional String topic, String key, String message, @Optional String propertyFileOverrides) throws ExecutionException, InterruptedException, EndpointException {
-		Properties properties = copyProperties(getProducerProperties());
-		if (null == topic){
-			topic = properties.getProperty(KafkaConnectorConstants.PRODUCER_TOPIC);
-		}
-		if (null == topic){
-			throw new EndpointException(MessageFactory.createStaticMessage("Topic cannot be null. Either pass in producer XMl or put 'producer.topic' in configuration file'"));
-		}
-		ProducerRecord producerRecord = new ProducerRecord(topic, key, message);
-		logger.debug("Using producer record {} with properties {}",producerRecord,properties);
-		List<ProducerRecord> producerRecords = Collections.singletonList(producerRecord);
-		KafkaProducerRunner runner = new KafkaProducerRunner(properties, producerRecords);
-		Future<List<RecordMetadata>> results = producerPool.submit(runner);
-		return results.get();
-	}
+
+    @Processor(name = "Producer", friendlyName = "Producer")
+    public List<RecordMetadata> producer(@Optional String topic, String key, String message, @Optional String propertyFileOverrides) throws ExecutionException, InterruptedException, EndpointException {
+        Properties properties = determineProperties(getProducerProperties(), propertyFileOverrides);
+        if (null == topic) {
+            topic = properties.getProperty(KafkaConnectorConstants.PRODUCER_TOPIC);
+        }
+        if (null == topic) {
+            throw new EndpointException(MessageFactory.createStaticMessage("Topic cannot be null. Either pass in producer XMl or put 'producer.topic' in configuration file'"));
+        }
+        ProducerRecord producerRecord = new ProducerRecord(topic, key, message);
+        logger.debug("Using producer record {} with properties {}", producerRecord, properties);
+        List<ProducerRecord> producerRecords = Collections.singletonList(producerRecord);
+        KafkaProducerRunner runner = new KafkaProducerRunner(properties, producerRecords);
+        Future<List<RecordMetadata>> results = producerPool.submit(runner);
+        return results.get();
+    }
 
 }
